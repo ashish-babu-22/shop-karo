50e41927de2550ec19907c47070df33dbd2977df
ashish-babu-22 <ashishbabu22april2003@gmail.com>
2023-08-03 13:03:31 +0530

    adding class files and sql script

--- SQLQuery/ShopKaro-SQL-Query.sql
+++ SQLQuery/ShopKaro-SQL-Query.sql
@@ -3,6 +3,13 @@ drop database 'shopkaro' if exist;
 create database 'shopkaro';
 use shopkaro;
 
+create table product_details(
+product_id integer not null auto_increment,
+name varchar(50) default null,
+quantity varchar(10) default null,
+price varchar(10) default null
+primary key(product_id),
+)auto_increment=1;
 
 create table order_details(
 order_id integer not null auto_increment,
@@ -10,17 +17,6 @@ Payment_mode varchar(30) default null,
 dod varchar(10) default null,
 primary key(order_id)
 )auto_increment=1;
-
-
-create table cart_details
-(
-cart_id integer not null auto_increment,
-total_price integer not null,
-order_id integer not null,
-primary key(cart_id),
-foreign key(order_id) references order_details(order_id)
-)auto_increment=1;
-
 
 create table credentials(
 cutomer_id integer not null auto_increment,
@@ -30,23 +26,27 @@ primary key(customer_id)
 )auto_increment=1;
 
 
+create table cart_details
+(
+cart_id integer not null auto_increment,
+total_price integer not null,
+product_id integer default null,
+order_id integer not null,
+primary key(cart_id),
+foreign key(product_id) references product_details(product_id),
+foreign key(order_id) references order_details(order_id)
+)auto_increment=1;
+
 create table customer(
-customer_id integer not null auto_increment,
+customer_id integer not null,
 name varchar(30) default null,
 contact varchar(15) default null,
 location varchar(20) default null,
 cart_id integer,
 foreign key(customer_id) references credentials(customer_id),
 foreign key(cart_id) references cart_details(cart_id)
-)auto_increment=1;
+);
 
-create table product_details(
-product_id integer not null auto_increment,
-name varchar(50) default null,
-quantity varchar(10) default null,
-price varchar(10) default null
-primary key(product_id),
-)auto_increment=1;
 
 create table review_details(
 review_id integer not null auto_increment,
