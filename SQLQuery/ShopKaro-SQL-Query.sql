1b6c184173f4c2317daa80901862988a26cc274d
ashish-babu-22 <ashishbabu22april2003@gmail.com>
2023-08-03 15:54:53 +0530

    modified sql

--- SQLQuery/ShopKaro-SQL-Query.sql
+++ SQLQuery/ShopKaro-SQL-Query.sql
@@ -11,31 +11,16 @@ price varchar(10) default null
 primary key(product_id),
 )auto_increment=1;
 
-create table order_details(
-order_id integer not null auto_increment,
-Payment_mode varchar(30) default null,
-dod varchar(10) default null,
-primary key(order_id)
-)auto_increment=1;
-
-create table credentials(
-cutomer_id integer not null auto_increment,
-mail varchar(50) not null auto_increment,
-password varchar(60) not null auto_increment,
-primary key(customer_id)
-)auto_increment=1;
-
 
 create table cart_details
 (
 cart_id integer not null auto_increment,
 total_price integer not null,
 product_id integer default null,
-order_id integer not null,
 primary key(cart_id),
 foreign key(product_id) references product_details(product_id),
-foreign key(order_id) references order_details(order_id)
 )auto_increment=1;
+
 
 create table customer(
 customer_id integer not null,
@@ -45,9 +30,18 @@ mail varchar(30) default null
 password varchar(30) default null,
 location varchar(20) default null,
 cart_id integer,
-foreign key(customer_id) references credentials(customer_id),
 foreign key(cart_id) references cart_details(cart_id)
 );
+
+
+create table order_details(
+order_id integer not null auto_increment,
+Payment_mode varchar(30) default null,
+dod varchar(10) default null,
+cart_id integer not null,
+primary key(order_id)
+foreign key (cart_id) references cart_details(cart_id)
+)auto_increment=1;
 
 
 create table review_details(
@@ -56,4 +50,20 @@ comments varchar(100) default null,
 primary key(review_id),
 foreign key(product_id) references product_details(product_id)
 )auto_increment=1;
+
+
+#for security
+create table credentials(
+username varchar(50) not null auto_increment,
+password varchar(60) not null auto_increment,
+enabled tinyint,
+primary key(username),
+);
+
+create table authorities(
+user varchar(30) not null,
+authority varchar(30) not null,
+unique key (user,authority)
+foreign key user references credentials(username)
+);
 
