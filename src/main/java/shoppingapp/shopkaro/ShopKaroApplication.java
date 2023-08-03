a735b75eaafcdda74865f3b27b29baadb655b2c1
ashish-babu-22 <ashishbabu22april2003@gmail.com>
2023-08-03 11:45:34 +0530

    Initial commit

---
+++ src/main/java/shoppingapp/shopkaro/ShopKaroApplication.java
@@ -0,0 +1,13 @@
+package shoppingapp.shopkaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopKaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shoppingapp/shopkaro/ShopKaroApplicationTests.java
@@ -0,0 +1,13 @@
+package shoppingapp.shopkaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopKaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
