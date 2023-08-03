a2809a55305d6cee56668a33f13b0e534217e906
ashish-babu-22 <ashishbabu22april2003@gmail.com>
2023-08-03 18:27:20 +0530

    changes in entity

--- src/main/java/Shopping_App/Shop_Karo/Entities/ProductDetails.java
+++ src/main/java/Shopping_App/Shop_Karo/Entities/ProductDetails.java
@@ -1,4 +1,70 @@
 package Shopping_App.Shop_Karo.Entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product_details")
 public class ProductDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    int id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name ="quantity")
+    String quantity;
+
+    @Column(name = "price")
+    String price;
+
+    public ProductDetails(String name, String quantity, String price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDetails{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity='" + quantity + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
 }
--- src/main/java/Shopping_App/Shop_Karo/Entities/ReviewsDetails.java
+++ src/main/java/Shopping_App/Shop_Karo/Entities/ReviewsDetails.java
@@ -1,4 +1,39 @@
 package Shopping_App.Shop_Karo.Entities;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "review_details")
 public class ReviewsDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    int id;
+
+    @Column(name = "comments")
+    String comments;
+
+    public ReviewsDetails(){}
+
+    public ReviewsDetails(String comments) {
+        this.comments = comments;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "ReviewsDetails{" +
+                "id=" + id +
+                ", comments='" + comments + '\'' +
+                '}';
+    }
 }
